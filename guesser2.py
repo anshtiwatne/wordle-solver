3b8b1158799537ae8bf65940aa59e746b1060be9
Ansh <ansh.tiwatne@gmail.com>
2022-01-27 12:08:05 +0530

    bug fixes

--- guesser2.py
+++
@@ -1,4 +0,0 @@
-"""
-Guessing logic for Wordle
-"""
-
--- scraper.py
+++ scraper.py
@@ -10,22 +10,18 @@ import playwright
 def get_hints(guess: str):
     """Replicate wordle behaviour:\nChecks a guess against the answer and only returns hints"""
 
-    word = "robot"
-    word_copy = copy(word)
+    word = "robot" # for now
     hints = {i: None for i in range(5)}
-
-    for i, letter in enumerate(guess):
-        # if letter is yellow
-        if letter in word and guess[i] != word[i]:
-            hints[i] = False
-            word = word.replace(letter, ".", 1)
-        # if lettter is green
-
-    word = copy(word_copy)
+    
     for i, letter in enumerate(guess):
         if letter in word and guess[i] == word[i]:
             hints[i] = True
             word = f"{word[:i]}.{word[i+1:]}"
-        # if letter is gray the hint remains unchanged
+
+    for i, letter in enumerate(guess):
+        if letter in word and guess[i] != word[i]:
+            if hints[i] != True:
+                hints[i] = False
+                word = word.replace(letter, ".", 1)
 
     return hints
