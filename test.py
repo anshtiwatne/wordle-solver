2eda08076f95161a083e525aa49de03df75a1fa4
Ansh <ansh.tiwatne@gmail.com>
2022-02-04 17:09:11 +0530

    added tests

--- test.py
+++ test.py
@@ -2,5 +2,49 @@ import unittest
 import guesser
 
 
-class WordGuesserTest(unittest.TestCase):
-    raise NotImplementedError
+class TestGuesser(unittest.TestCase):
+    """Tests for guesser.py"""
+
+    def test_hints(self):
+        """Test the get_hints function"""
+
+        guesser.CHECK_WORD = "aabbb"
+
+        guess = "aaabb"
+        self.assertEqual(guesser.get_hints(guess), {
+            0: True,
+            1: True,
+            2: None,
+            3: True,
+            4: True
+        })
+        guess = "bbaaa"
+        self.assertEqual(guesser.get_hints(guess), {
+            0: False,
+            1: False,
+            2: False,
+            3: False,
+            4: None
+        })
+        guess = "later"
+        self.assertEqual(guesser.get_hints(guess), {
+            0: None,
+            1: True,
+            2: None,
+            3: None,
+            4: None
+        })
+
+    def test_guesses(self):
+        """Test the guesses from the main function"""
+
+        guesser.CHECK_WORD = "check"
+        guesses = []
+        for guess in guesser.main():
+            guesses.append(guess)
+        last_guess = guesses[-1][0]
+        self.assertEqual(guesser.CHECK_WORD, last_guess)
+
+
+if __name__ == "__main__":
+    unittest.main()
